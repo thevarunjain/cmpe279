97bd0ca52bfe97b815045e6ee50a94ece7b48006
Varun Jain <varunsj18@gmail.com>
2020-05-06 15:44:55 -0700

    Assignment 1

---
+++ Assignment 1/server.c
@@ -0,0 +1,89 @@
+// Server side C/C++ program to demonstrate Socket programming
+
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 8080
+
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    // This is optional but it helps in reuse of address (release after use)
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+
+    // Setting host and port in address struct
+    // INADDR_ANY will bind with localhost
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    // Binding socket with address struct configured
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+
+    // Start server socket for listening  
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    // Establish connection with client
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+
+    // Create a 
+    int pid = fork();
+
+    if(pid == 0){        // child will execute this part
+
+        // run this on bash to get nobody id 
+        // $ cat /etc/passwd | grep nobody
+        int droppedPid = setuid(65534);
+
+        valread = read( new_socket , buffer, 1024); 
+        printf("%s\n",buffer ); 
+        send(new_socket , hello , strlen(hello) , 0 ); 
+        printf("Hello message sent\n"); 
+
+    }else{          // parent will execute this part , id > 0
+
+        wait(NULL);         // waits for all child process to finish 
+        printf("Parent is now terminating, as child process is done\n");
+    }
+
+    return 0; 
+} 
